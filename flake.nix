faf001ccc75dfe7ae1f1eb9a0c1d1d5a8b471cf9
Robert Ormandi <ormandi.robert@gmail.com>
2025-10-02 11:11:39 -0500

    WIP

--- flake.nix
+++ flake.nix
@@ -10,36 +10,36 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        
+
         tmux-mem-cpu-load = pkgs.stdenv.mkDerivation {
           pname = "tmux-mem-cpu-load";
-          version = "show_cpu_show_ram";
-          
+          version = "2037bfd0c15b019346996331722714d324086767";
+
           src = pkgs.fetchFromGitHub {
             owner = "ormandi";
             repo = "tmux-mem-cpu-load";
             rev = "2037bfd0c15b019346996331722714d324086767";
-            sha256 = "sha256-E2oDoqKcCS+T1XBD4+2YrCYGZjyk8gb1h0iyxQxN0HA=";
+            hash = "sha256-0w6h9l6cbcj8hzshdwm47ik0c9mck3ny6hvhsn9jy2cwlai06shk";
           };
-          
+
           nativeBuildInputs = [ pkgs.cmake ];
-          
+
           buildInputs = [ ];
-          
+
           # Standard CMake build
           configurePhase = ''
             cmake .
           '';
-          
+
           buildPhase = ''
             make
           '';
-          
+
           installPhase = ''
             mkdir -p $out/bin
             cp tmux-mem-cpu-load $out/bin/
           '';
-          
+
           meta = with pkgs.lib; {
             description = "CPU, RAM memory, and load monitor for use with tmux";
             homepage = "https://github.com/ormandi/tmux-mem-cpu-load/tree/show_cpu_show_ram";
@@ -47,17 +47,17 @@
             platforms = platforms.unix;
           };
         };
-        
+
       in {
         packages.default = tmux-mem-cpu-load;
         packages.tmux-mem-cpu-load = tmux-mem-cpu-load;
-        
+
         devShells.default = pkgs.mkShell {
           buildInputs = [
             tmux-mem-cpu-load
             pkgs.tmux
           ];
-          
+
           shellHook = ''
             echo "tmux-mem-cpu-load is available"
           '';
